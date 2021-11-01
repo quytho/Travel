68765a726609a0a2ac58bf00d69c298a08e2a910
vanquytho12345@gmail.com <59798610+quytho@users.noreply.github.com>
2021-11-01 22:30:51 +0700

    modifer slide

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,8 @@ $(".category-slide").slick({
   dots: false,
   draggable: false,
   adaptiveHeight: false,
-  prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./image/arrow -left.png"/></button>`,
-  nextArrow: `<button type='button' class='slick-next pull-right'><img src="./image/arrow-right.png"/></button>`,
+  prevArrow: `<button type='button' class='slick-prev pull-left'><img src="../image/arrow -left.png"/></button>`,
+  nextArrow: `<button type='button' class='slick-next pull-right'><img src="../image/arrow-right.png"/></button>`,
   responsive: [
     {
       breakpoint: 1024,
