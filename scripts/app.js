ee0f2edfa918f2524b7224cd22c5c2e20e992af3
vanquytho12345@gmail.com <59798610+quytho@users.noreply.github.com>
2021-11-02 15:08:10 +0700

    toogle menu

--- scripts/app.js
+++ scripts/app.js
@@ -30,3 +30,15 @@ $(".category-slide").slick({
     },
   ],
 });
+
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const activeClass = "is-show";
+toggle.addEventListener("click", function () {
+  menu.classList.add(activeClass);
+});
+window.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+    menu.classList.remove(activeClass);
+  }
+});
